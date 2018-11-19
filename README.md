d156dddf6d7ff1cd8d0c97b39128fff5094c0320
Henrik Jepsen <hjep@tv2.dk>
2018-11-19 13:34:39 +0100

    2nd commit

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # JavaFX 11
 
 Tryout on JavaFX11 and Gradle build
+
+## Docs
+
+[openJFX docs](https://openjfx.io/openjfx-docs/#introduction)
+
+## Run
+
+```sh
+gradle jlink
+build/hellofx/bin/java -m hellofx/hellofx.HelloFX
+```
+
--- src/main/java/hellofx/HelloFX.java
+++ src/main/java/hellofx/HelloFX.java
@@ -1,12 +1,10 @@
+package hellofx;
+
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
